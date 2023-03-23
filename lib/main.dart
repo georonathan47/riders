2da14bd87f3810415c1dbcc81a29e412c161d04c
George Osafo-Osei Jonathan <georonathan47@gmail.com>
2023-03-23 10:04:05 +0000

    🚀

--- lib/main.dart
+++ lib/main.dart
@@ -231,7 +231,7 @@ class _RidersState extends State<Riders> {
           foregroundColor: Colors.white,
         ),
       ),
-      home:  const SplashScreen(),
+      home: const SplashScreen(),
       // home: const Login(),
     );
   }
